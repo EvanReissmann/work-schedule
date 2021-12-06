cf35cf2406dbe9cdd3b788a08eef9368291c0ac5
Evan Reissmann <evanreissmann4@yahoo.com>
2021-12-05 22:55:52 -0500

    update again

--- script.js
+++ script.js
@@ -2,20 +2,20 @@
 var currentDate = moment().format('dddd') + " " + moment().format("Do MMM YYYY");
 // added tasks
 var tasks = {
-    "9am": [],
-    "10am": [],
-    "11am": [],
-    "12pm": [],
-    "13pm": [],
-    "14pm": [],
-    "15pm": [],
-    "16pm": [],
-    "17pm": [],
+    9: [],
+    10: [],
+    11: [],
+    12: [],
+    13: [],
+    14: [],
+    15: [],
+    16: [],
+    17: [],
 }
 // Created functions for the task
-function saveTask9am () {
-    var task = $("#9am").val();
-    tasks['9am'] = task;
+function saveTask9 () {
+    var task = $("#9").val();
+    tasks[9] = task;
     saveTaskLocalStorage();
 }
 
@@ -24,10 +24,24 @@ function saveTaskLocalStorage() {
 }
 
 function currentTime() {
+    var currentTime = new Date().getHours();
+    $(".form-control").each(function() {
+        var taskTime=$(this).attr('id');
+        if ( currentTime > taskTime) {
+            $(this).addClass("blacked-out");
+        }
+      });
+    
+}
 
 function getTaskLocalStorage() {
     tasks = JSON.parse(localStorage.getItem("tasks"));
-    console.log(tasks);
+    $(".form-control").each(function() {
+        var taskTime=$(this).attr('id');
+        $("#"+taskTime)
+
+      });
 }
 // called the local storage
 getTaskLocalStorage();
+currentTime();
